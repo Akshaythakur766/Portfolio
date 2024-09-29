37571edf84a738851f351c75d4c884852edb9afb
Akshaythakur766 <167159617+Akshaythakur766@users.noreply.github.com>
2024-09-29 21:34:21 +0530

    CTA & Footer & Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 import { Header } from "@/sections/Header"
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -14,6 +16,8 @@ export default function Home() {
       <TapeSection/>
       <TestimonialsSection/>
       <AboutSection/>
+      <ContactSection/>
+      <Footer/>
     </div>
   );
 }
